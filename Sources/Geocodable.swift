b70d7da6c3c70b1674bb6facf6dd00bd61108d11
htinlinn <linn@prolificinteractive.com>
2016-12-15 14:19:26 -0500

    Added support for location coordinates

--- Sources/Geocodable.swift
+++ Sources/Geocodable.swift
@@ -22,12 +22,8 @@ public protocol Geocodable {
     func geocodeLocality(_ string: String,
                          forCountry country: String,
                          completion: @escaping (Result<GeocodedAddress, GeocodingError>) -> ())
-
-}
-
-public extension Geocodable {
-
-    /// Geocodes the given string into a geocoded address.
+    
+    /// Geocodes the given string into a geocoded location.
     ///
     /// - Parameters:
     ///   - string: The string to geocode.
@@ -35,38 +31,68 @@ public extension Geocodable {
     ///   - completion: The completion block.
     func geocodeLocality(_ string: String,
                          forCountry country: String,
+                         completion: @escaping (Result<GeocodedLocation, GeocodingError>) -> ())
+
+}
+
+public extension Geocodable {
+
+    func geocodeLocality(_ string: String,
+                         forCountry country: String,
                          completion: @escaping (Result<GeocodedAddress, GeocodingError>) -> ()) {
         geocode(string, forCountry: country) { result in
-            guard let placemark = result.value else {
-                completion(.failure(.error("Couldn't find a matching address")))
-                return
-            }
-
-            let locality = placemark.formattedLocality()
-            let state = placemark.administrativeArea
-
-            if let locality = locality, let state = state {
-                let address = GeocodedAddress(locality: locality, state: state)
-                completion(.success(address))
-            } else {
-                completion(.failure(.error("Couldn't find a matching address")))
-            }
+            completion(result.flatMap(self.geocodedAddressFromPlacemark))
         }
     }
+    
+    func geocodeLocality(_ string: String,
+                         forCountry country: String,
+                         completion: @escaping (Result<GeocodedLocation, GeocodingError>) -> ()) {
+        geocode(string, forCountry: country) { result in
+            completion(result.flatMap(self.geocodedLocationFromPlacemark))
+        }
+    }
+    
+    // MARK: - Private functions
 
     private func geocode(_ string: String, forCountry country: String, completion: @escaping (Result<CLPlacemark, GeocodingError>) -> ()) {
         let geocoder = CLGeocoder()
+        
         geocoder.geocodeAddressString(string) { (placemarks, error) in
             if let error = error {
                 completion(.failure(.error(error.localizedDescription)))
             } else if let placemarks = placemarks {
                 let usaPlacemarks = placemarks.filter { $0.isoCountryCode == country }
+                
                 if let placemark = usaPlacemarks.first {
                     completion(.success(placemark))
                 } else {
-                    completion(.failure(.error("Couldn't find a matching address")))
+                    completion(.failure(.error("Couldn't find a matching address in specified country.")))
                 }
             }
+        }
+    }
+    
+    private func geocodedAddressFromPlacemark(_ placemark: CLPlacemark) -> Result<GeocodedAddress, GeocodingError> {    
+        return Result {
+            guard let locality = placemark.formattedLocality(), let state = placemark.administrativeArea else {
+                throw GeocodingError.error("Couldn't find a matching locality.")
+            }
+            
+            return GeocodedAddress(locality: locality, state: state)
+        }
+    }
+    
+    private func geocodedLocationFromPlacemark(_ placemark: CLPlacemark) -> Result<GeocodedLocation, GeocodingError> {
+        return Result {
+            guard let coordinate = placemark.location?.coordinate else {
+                throw GeocodingError.error("Couldn't find matching coordinates.")
+            }
+            
+            let address = try geocodedAddressFromPlacemark(placemark).dematerialize()
+            return GeocodedLocation(latitude: coordinate.latitude,
+                                    longitude: coordinate.longitude,
+                                    address: address)
         }
     }
     
